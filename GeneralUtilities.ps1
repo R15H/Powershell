63ac141ba3a1e2be60142e7e8450e043359b1467
Alexandre Bento <alexandre.e.bento@tecnico.ulisboa.pt>
2021-11-09 15:37:40 +0000

    PDF fully working, added merge functionality

--- GeneralUtilities.ps1
+++ GeneralUtilities.ps1
@@ -1,97 +1,131 @@
 
-function Import-Excel {
 
+
+function Convert-ExcelToCsvFile {
+    [CmdletBinding()]
+    param([string]$Path, [string]$DestinationFolder)
+    $Path = Resolve-Path $Path
+    $DestinationFolder = $DestinationFolder ? (Resolve-Path $DestinationFolder) : (New-TemporaryFile)
+
+    $csvFiles = [System.Collections.ArrayList]::new()
+    $Excel = New-Object -ComObject Excel.Application 
+    $Excel.DisplayAlerts = $false # remove prompts to substitute file
+    $book = $Excel.Workbooks.Open($Path)
+    $i = 0
+    write-host "Reading sheets:" -NoNewLine
+    foreach ($sheet in $book.Worksheets) {
+        $savePath = Join-Path $DestinationFolder  "${split-path $path -LeafBase}$i.csv"
+        write-host " $0" -NoNewLine
+        $csvFiles.add($savePath)
+        $sheet.saveAs($savePath, 6) # 6 to save as csv
+        $i += 1
+    }
+    $Excel.Quit() | Out-Null 
+    $csvFiles
 }
 
-
-function -Csv {
+function  Convert-PDFToCsvFile {
     [CmdletBinding()]
-    param([string]$Path, [int]$ShowFirst, [int]$TrimFirst, [switch]$Supress)
+    param([parameter(Position = 0)][string]$Path, [parameter(Position = 1)][string]$DestinationFolder, [switch]$Internals) 
+    $Path = Resolve-Path $Path
+    $DestinationFolder = $DestinationFolder ? (Resolve-Path $DestinationFolder) : (New-TemporaryFile)
+    
+    $fname = Split-Path $Path -LeafBase
+    $outfile = "$DestinationFolder/$fname"
+    py -c "import tabula; tabula.convert_into(r'$($Path.replace('\','/'))',r'$($outfile.replace('\','/'))', output_format = 'csv', pages='all')"   
+    $outfile
+}
+
+function Convert-FileToCsv {
+    [CmdletBinding()]
+    param([string]$Path, [int]$First, [int]$TrimFirst, [switch]$Supress, [int]$MergeEnd, [switch]$NoCache)
     $src = Resolve-Path $Path 
     $fileName = Split-Path $Path -LeafBase
     $tempPath = [System.io.Path]::GetTempPath()
     $cache = Join-Path $tempPath "$fileName.cachy"
-    $csvFiles = [System.Collections.ArrayList]::new()
     $csvObjects = [System.Collections.ArrayList]::new()
 
     $hasCachedConversion = ((Test-Path $cache) -and ( (Get-Item $cache).lastWriteTime -gt (Get-Item $src).lastWriteTime))
-
-    if (-not $hasCachedConversion) {
-        $csvFiles = Convert-ExcelToCsv -Path $src -DestinationFolder $tempPath
-    } else {
+    if ( (-not $hasCachedConversion) -or $NoCache) {
+        $extension = Split-Path $src -Extension 
+        switch ($extension) {
+            '.pdf' { 
+                write-host "Processing file as a pdf..."
+                $csvFiles = Convert-PDFToCsvFile -Path $src -DestinationFolder $tempPath
+                break
+            }
+            Default {
+                Write-Warning "Filetype not matched, processing file as an excel..."
+                $csvFiles = Convert-ExcelToCsvFile -Path $src -DestinationFolder $tempPath
+            }
+        }
+        $csvFiles | Export-Clixml -Path $cache  
+    }
+    else {
         $csvFiles = Import-CliXml -Path $cache
     }
 
 
     foreach ($file in $csvFiles) {
-        $finalCSVFile = $file
-        $content = Get-Content $finalCSVFile
-        if($ShowFirst){
-            $content | select -First $ShowFirst | write-host 
+        $content = Get-Content $file
+        $finalContent = $content
+        $finalFile = (New-TemporaryFile).FullName
+
+        if ($trimFirst) {
+            #antes estava a meter aqui que $finalFile era um tempory object
+            #acontece que, como esta operação estava a ser feita eninhadamente
+            #depois pensava que estava a usar um file e estava a usar um outro, 
+            #alterado por um if obscuro no meio do codigo! ( nem precisa ser obscuro, se estou a trabalhar num scope mais a cima o meu cerebro ignora scopes não relevantes)
+            #o melhor é sempre fazer alterações de uma variavel no mesmo scope -> usar funções para isto
+            #em que ela é criada!! 
+            $finalContent = ($finalContent | Select-Object -skip $trimFirst)
         }
-        if($trimFirst){
-            $finalCSVFile = New-TemporaryFile
-            set-content $finalCSVFile -Value ($content | select -skip $trimFirst)
+
+        # merges rows [starting from the index MergeStart to MergeEnd] and trims all rows before the merge 
+        if ($MergeEnd) {
+            $MergeStart ??= 0
+
+            write-host "$($finalContent[0])"
+            write-host "$($finalContent[1])"
+            write-host "$($finalContent[2])"
+            write-host "$($finalContent[3])"
+            $mergedRow = [System.Collections.ArrayList]$finalContent[$MergeStart].split(',')
+            write-host "MergedRow before: $($mergedRow -join ',')"
+            foreach ($row in $finalContent[($MergeStart + 1)..$MergeEnd]) {
+                $i = 0
+                write-host $row
+                foreach ($value in $row.split(',')) {
+                    if ($mergedRow.count -lt ($i+1)) {
+                        $mergedRow.add(" $value")
+                    } else {
+                        $mergedRow[$i] += " $value"
+                    }
+                    $i += 1
+                }
+            }
+            $content[$MergeEnd] = $mergedRow.trim() -join ',' #trim is important to access properties conveniently
+            $finalContent = $content[$MergeEnd..$content.Length] 
+            write-host "MergedRow after: $($mergedRow -join ',')"
         }
-        $csvObj = Import-CSV $finalCSVFile
+        if ($First) {
+            $finalContent | Select-Object -First $First | write-host 
+        }
+
+
+
+        set-content $finalFile -Value $finalContent 
+        #set-content -Path $finalCSVFile -Value $finalCSVValue 
+        $csvObj = Import-CSV $finalFile
         #$csvObj | Add-Member -NotePropertyName 'FromOriginalFile' -NotePropertyValue $file 
         #$csvObj | Add-Member -NotePropertyName 'FromFile' -NotePropertyValue $finalCSVFile
-        $csvObjects.add($csvObj)
+        $csvObjects.add($csvObj) | Out-Null
     }
-    if(-not $supress -and (-not $ShowFirst)){
+    if (-not $supress -and (-not $First)) {
         $csvObjects
     }
 }
-function Convert-ExcelToCsv {
-    if (-not $hasCachedConversion) {
-        $Excel = New-Object -ComObject Excel.Application 
-        $Excel.DisplayAlerts = $false # remove prompts to substitute file
-        $book = $Excel.Workbooks.Open($src)
-        $i = 0
-        write-host "Reading sheets:" -NoNewLine
-        foreach ($sheet in $book.Worksheets) {
-            $savePath = Join-Path $tempPath  "$fileName$i.csv"
-            write-host " $0" -NoNewLine
-            $csvFiles.add($savePath)
-            $sheet.saveAs($savePath, 6) # 6 to save as csv
-            $i += 1
-        }
-        Export-CliXml -Path $cache -InputObject $csvFiles 
-        Start-Job $Excel.Quit()
-    }
-}
 
-function Convert-ExcelToCsv{
-    [CmdletBinding()]
-    param([string]$Path,[string]$DestinationFolder, [int]$ShowFirst, [int]$TrimFirst, [switch]$Supress)
-    $src = Resolve-Path $Path 
-    $fileName = Split-Path $Path -LeafBase
-    $tempPath = [System.io.Path]::GetTempPath()
-    $cache=  Join-Path $tempPath "$fileName.cachy"
-    $csvFiles = [System.Collections.ArrayList]::new()
-    $csvObjects = [System.Collections.ArrayList]::new()
-
-    $hasCachedConversion = ((Test-Path $cache) -and ( (Get-Item $cache).lastWriteTime -gt (Get-Item $src).lastWriteTime))
-    if(-not $hasCachedConversion){
-        $Excel = New-Object -ComObject Excel.Application 
-        $Excel.DisplayAlerts = $false # remove prompts to substitute file
-        $book = $Excel.Workbooks.Open($src)
-        $i = 0
-        write-host "Reading sheets:" -NoNewLine
-        foreach ($sheet in $book.Worksheets) {
-            $savePath = Join-Path $DestinationFolder  "$fileName$i.csv"
-            write-host " $0" -NoNewLine
-            $csvFiles.add($savePath)
-            $sheet.saveAs($savePath, 6) # 6 to save as csv
-            $i +=1
-        }
-        Export-CliXml -Path $cache -InputObject $csvFiles 
-        Start-Job $Excel.Quit()
-    } else {
-        $csvFiles = Import-CliXml -Path $cache
-    }
-
-}
-
- #Convert-ExcelToCsv '~/Downloads/Pauta - 2021 (8).xlsx' -OutVariable som -ShowFirst 5 -TrimFirst 2 -Supress
- #Find-ISTPerson -ID -Name  -ShowPhoto --> returns json 
+#Convert-ExcelToCsvFile -Path '~/Downloads/Pauta - 2021 (8).xlsx' -DestinationFolder '.' 
+#Convert-FileToCsv '~/Downloads/Pauta - 2021 (8).xlsx' -OutVariable som -First 5 -TrimFirst 2 -Supress
+#Convert-PDFToCsvFile -Path 'C:\Users\Tester\Downloads\notas\arquitetura de computadores\pauta-AC-2019-20-v61.pdf' -DestinationFolder '.' 
+#Find-ISTPerson -ID -Name  -ShowPhoto --> returns json 
